4fc3b7e8447b976c7f58211ca342f351095272bd
Keerthana-172 <eerapogukeerthana@gmail.com>
2025-07-24 09:50:06 +0530

    Add files via upload

--- api/train_model.py
+++ api/train_model.py
@@ -5,26 +5,25 @@ import joblib
 
 # Create data
 data = {
-    'Math': [78, 45, 90, 35, 65, 82, 30, 40, 55, 92],
-    'Science': [69, 40, 95, 30, 70, 75, 20, 45, 60, 98],
-    'English': [72, 39, 88, 25, 60, 85, 28, 50, 58, 93],
-    'Result': ['Pass', 'Fail', 'Pass', 'Fail', 'Pass', 'Pass', 'Fail', 'Fail', 'Pass', 'Pass']
+    "Math": [78, 45, 90, 35, 65, 82, 30, 40, 55, 92],
+    "Science": [69, 40, 95, 30, 70, 75, 20, 45, 60, 98],
+    "English": [72, 39, 88, 25, 60, 85, 28, 50, 58, 93],
+    "Result": ["Pass", "Fail", "Pass", "Fail", "Pass", "Pass", "Fail", "Fail", "Pass", "Pass"]
 }
 
 # Create DataFrame
 df = pd.DataFrame(data)
 df['Result'] = df['Result'].map({'Pass': 1, 'Fail': 0})
+#print(df)
 
 # Features and label
 X = df[['Math', 'Science', 'English']]
 Y = df['Result']
-
-# Split the data
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 # Train model
 model = LogisticRegression()
 model.fit(x_train, y_train)
 
-# Save model
-joblib.dump(model, 'model.pkl')
+# Save model(pkl)
+joblib.dump(model, "model.pkl")
